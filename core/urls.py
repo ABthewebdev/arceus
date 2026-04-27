19cd680c26fbbf4bd418d637d9c62444ebfbd46a
Alex Brewster <alexgbrewster@gmail.com>
2026-04-27 14:18:25 -0400

    new restaurant login page

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.restaurant_owner_register, name='restaurant_register'),
     path('browse/', views.browse_food, name='browse_food'),
     path('restaurant/', views.restaurant_dashboard, name='restaurant_dashboard'),
     path('restaurant/add/', views.add_restaurant, name='add_restaurant'),
