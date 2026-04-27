627cabf9cb4f3674fbbedec648cd99825bdf16a7
Alex Brewster <alexgbrewster@gmail.com>
2026-04-27 01:46:29 -0400

    testing

--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods
 from .models import Restaurant, Food, Order
 
